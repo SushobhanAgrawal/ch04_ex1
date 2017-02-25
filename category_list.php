556a70c770d0fc4faf8d356e2e7e25a6bf658346
Sushobhan Agrawal <sa929@njit.edu>
2017-02-24 22:06:52 -0500

    added delete_category.php

--- category_list.php
+++ category_list.php
@@ -25,16 +25,20 @@ $statement->closeCursor();
     <h1>Category List</h1>
     <table>
         <tr>
+            <th>Category_ID</th>
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>
         
         <?php foreach ($categories as $category) : ?>
         <tr>
+          <td><?php echo $category['categoryID']; ?></td>
           <td><?php echo $category['categoryName']; ?></td>
-          <td>
+          <td><form action="delete_category.php" method="post">
+              <input type="hidden" name="category_id"
+                      value="<?php echo $category['categoryID']; ?>">
               <input type="submit" value="Delete">
-          </td>    
+          </form></td>    
         </tr>
         
     <?php endforeach; ?>    
